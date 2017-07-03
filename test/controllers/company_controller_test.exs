33edf4d1ebe9dcfb88596311f28c6f66753bf5db
Jon Lunsford <jon@capturethecastle.net>
2017-07-02 21:43:33 -0700

    Implement Company.index/3

--- test/controllers/company_controller_test.exs
+++ test/controllers/company_controller_test.exs
@@ -1,4 +1,38 @@
 defmodule Inventory.CompanyControllerTest do
   use ExUnit.Case
   use Inventory.ConnCase, async: true
+
+  alias Inventory.User
+  alias Inventory.Company
+
+  @valid_user_attrs %{
+    email: "test@example.com",
+    password: "password",
+    password_confirmation: "password"
+  }
+
+  describe "index/3" do
+    test "Redirects to Session.new if user is not logged in", %{conn: conn} do
+      conn = get conn, company_path(conn, :index)
+
+      assert redirected_to(conn, 401) =~ session_path(conn, :new)
+    end
+
+    test "Lists companies owned by user if user is logged in", %{conn: conn} do
+      company = Company.changeset(%Company{}, %{title: "Owned"})
+      user =
+        User.changeset(%User{}, @valid_user_attrs)
+        |> Ecto.Changeset.put_assoc(:companies, [company])
+        |> Repo.insert!
+
+      conn =
+        conn
+        |> sign_in(user)
+        |> get(company_path(conn, :index))
+
+      assert html_response(conn, 200)
+      assert Enum.count(conn.assigns.companies) == 1
+      assert List.first(conn.assigns.companies) == List.first(user.companies)
+    end
+  end
 end
--- web/controllers/company_controller.ex
+++ web/controllers/company_controller.ex
@@ -1,3 +1,14 @@
 defmodule Inventory.CompanyController do
   use Inventory.Web, :controller
+  use Inventory.ApplicationController
+
+  alias Inventory.Company
+
+  def index(conn, _params, current_user) do
+    companies = Company
+                |> where(owner_id: ^current_user.id)
+                |> Repo.all
+
+    render(conn, :index, companies: companies)
+  end
 end
