15b2f3ca7e6d11dbd4aa5936cc4d30d839f5de43
Jon Lunsford <jon@capturethecastle.net>
2017-07-03 14:23:46 -0700

    Add company controller index and view

--- test/controllers/company_controller_test.exs
+++ test/controllers/company_controller_test.exs
@@ -34,5 +34,18 @@ defmodule Inventory.CompanyControllerTest do
       assert Enum.count(conn.assigns.companies) == 1
       assert List.first(conn.assigns.companies) == List.first(user.companies)
     end
+
+    test "Returns 200 ok if user is logged in", %{conn: conn} do
+      user =
+        User.changeset(%User{}, @valid_user_attrs)
+        |> Repo.insert!
+
+      conn =
+        conn
+        |> sign_in(user)
+        |> get(company_path(conn, :index))
+
+      assert html_response(conn, 200)
+    end
   end
 end
