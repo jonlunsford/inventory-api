2290783835465bfd4d77338a99c4985b6392cacc
Jon Lunsford <jon@capturethecastle.net>
2017-07-02 22:13:29 -0700

    Fix typo

--- lib/guardian_serializer.ex
+++ lib/guardian_serializer.ex
@@ -8,5 +8,5 @@ defmodule Inventory.GuardianSerializer do
   def for_token(_), do: { :error, "Unknown resource type" }
 
   def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
-  def frim_token(_), do: { :error, "Unknown resource type" }
+  def from_token(_), do: { :error, "Unknown resource type" }
 end
