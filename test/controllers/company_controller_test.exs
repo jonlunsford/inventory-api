e0d446d86bef74861c89f88287712c67fd13bcbc
Jon Lunsford <jon@capturethecastle.net>
2017-07-03 22:09:53 -0700

    Refactor logged in logic

--- test/controllers/company_controller_test.exs
+++ test/controllers/company_controller_test.exs
@@ -13,24 +13,35 @@ defmodule Inventory.CompanyControllerTest do
 
   @valid_attrs %{title: "some company title"}
 
+  setup %{conn: conn} do
+    user =
+      case Repo.get_by(User, email: "test@test.com") do
+        {:ok, result} -> result
+        nil ->
+          User.changeset(%User{}, @valid_user_attrs)
+          |> Ecto.Changeset.put_assoc(:companies, [Company.changeset(%Company{}, %{title: "Owned"})])
+          |> Repo.insert!
+      end
+
+    conn =
+      conn
+      |> sign_in(user)
+
+    {:ok, %{conn: conn, user: user}}
+  end
+
   describe "index/3" do
     test "Redirects to Session.new if user is not logged in", %{conn: conn} do
-      conn = get conn, company_path(conn, :index)
+      conn =
+        conn
+        |> Guardian.Plug.sign_out
+        |> get(company_path(conn, :index))
 
       assert redirected_to(conn, 401) =~ session_path(conn, :new)
     end
 
-    test "Lists companies owned by user if user is logged in", %{conn: conn} do
-      company = Company.changeset(%Company{}, %{title: "Owned"})
-      user =
-        User.changeset(%User{}, @valid_user_attrs)
-        |> Ecto.Changeset.put_assoc(:companies, [company])
-        |> Repo.insert!
-
-      conn =
-        conn
-        |> sign_in(user)
-        |> get(company_path(conn, :index))
+    test "Lists companies owned by user if user is logged in", %{conn: conn, user: user} do
+      conn = get conn, company_path(conn, :index)
 
       assert html_response(conn, 200)
       assert Enum.count(conn.assigns.companies) == 1
@@ -38,14 +49,7 @@ defmodule Inventory.CompanyControllerTest do
     end
 
     test "Returns 200 ok if user is logged in", %{conn: conn} do
-      user =
-        User.changeset(%User{}, @valid_user_attrs)
-        |> Repo.insert!
-
-      conn =
-        conn
-        |> sign_in(user)
-        |> get(company_path(conn, :index))
+      conn = get conn, company_path(conn, :index)
 
       assert html_response(conn, 200)
     end
@@ -53,28 +57,14 @@ defmodule Inventory.CompanyControllerTest do
 
   describe "create/3" do
     test "with valid attributes creates and redirects to index/3", %{conn: conn} do
-      user =
-        User.changeset(%User{}, @valid_user_attrs)
-        |> Repo.insert!
-
-      conn =
-        conn
-        |> sign_in(user)
-        |> post(company_path(conn, :create), %{company: @valid_attrs})
+      conn = post conn, company_path(conn, :create), %{company: @valid_attrs}
 
       assert redirected_to(conn, 201) =~ company_path(conn, :index)
       assert Repo.get_by(Company, @valid_attrs)
     end
 
     test "withh invalid attributes renders new/3", %{conn: conn} do
-      user =
-        User.changeset(%User{}, @valid_user_attrs)
-        |> Repo.insert!
-
-      conn =
-        conn
-        |> sign_in(user)
-        |> post(company_path(conn, :create), %{company: %{title: ""}})
+      conn = post conn, company_path(conn, :create), %{company: %{title: ""}}
 
       assert html_response(conn, 422)
       refute Enum.empty?(conn.assigns.changeset.errors)
@@ -82,35 +72,17 @@ defmodule Inventory.CompanyControllerTest do
   end
 
   describe "update/3" do
-    test "with valid attributes updates and redirects to index/3", %{conn: conn} do
-      company = Company.changeset(%Company{}, %{title: "Owned"})
-      user =
-        User.changeset(%User{}, @valid_user_attrs)
-        |> Ecto.Changeset.put_assoc(:companies, [company])
-        |> Repo.insert!
-
+    test "with valid attributes updates and redirects to index/3", %{conn: conn, user: user} do
       company = List.first(user.companies)
-      conn =
-        conn
-        |> sign_in(user)
-        |> patch(company_path(conn, :update, company), %{company: %{title: "Update"}})
+      conn = patch conn, company_path(conn, :update, company), %{company: %{title: "Update"}}
 
       assert redirected_to(conn, 200) =~ company_path(conn, :index)
       assert Repo.get_by(Company, %{title: "Update"})
     end
 
-    test "with invalid attributes renders edit/3", %{conn: conn} do
-      company = Company.changeset(%Company{}, %{title: "Owned"})
-      user =
-        User.changeset(%User{}, @valid_user_attrs)
-        |> Ecto.Changeset.put_assoc(:companies, [company])
-        |> Repo.insert!
-
+    test "with invalid attributes renders edit/3", %{conn: conn, user: user} do
       company = List.first(user.companies)
-      conn =
-        conn
-        |> sign_in(user)
-        |> patch(company_path(conn, :update, company), %{company: %{title: ""}})
+      conn = patch conn, company_path(conn, :update, company), %{company: %{title: ""}}
 
       assert html_response(conn, 422)
       refute Enum.empty?(conn.assigns.changeset.errors)
